36f0cf5b9e4834b714e4e7a9568bb978ecd94efb
Evgeniy_Grokholskiy <grokholskiy@gmail.com>
2021-09-29 17:14:13 +0500

    new project

---
+++ js/partners.js
@@ -0,0 +1,7 @@
+(function partners () {
+    const root = document.querySelector('.partners');
+    root.innerHTML = `
+            <p class="partners__text">НАШИ ПАРТНЕРЫ</p>
+            <div class="partners__list"></div>
+    `
+})()
