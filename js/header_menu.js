3a679d17148eca07e325ab2d4019d5e65d5db199
Evgeniy_Grokholskiy <grokholskiy@gmail.com>
2021-10-05 20:06:20 +0500

    made a mobile menu, finished responsive output

--- js/header_menu.js
+++ js/header_menu.js
@@ -1,16 +1,23 @@
 document.querySelector('.header__nav-arrow').addEventListener('click', ()=> {
-    const liArray = document.querySelectorAll('.header__list_item');
+
+    const liArray = document.querySelectorAll('.header__list_item-mobile');
     const navMobile = document.querySelector('.header__nav-mobile');
     const navArrow = document.querySelector('.header__nav-arrow');
     navArrow.classList.toggle('header__nav-arrow-open');
     navMobile.classList.toggle('show');
-    if (navMobile.classList.contains('show')){
-        liArray.forEach((element) => {
-            element.classList.add('on');
-        })
-    }else {
-        liArray.forEach(((element) => {
-            element.classList.remove('on');
-        }))
-    }
+
+
+        if (navMobile.classList.contains('show')){
+            setTimeout(()=> {
+            liArray.forEach((element) => {
+                element.classList.add('on');
+            })
+            },200)
+        }else {
+            liArray.forEach(((element) => {
+                element.classList.remove('on');
+            }))
+        }
+
 })
+
