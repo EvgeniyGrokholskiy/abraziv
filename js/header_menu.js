d4fa9a752b64e63ed4d7ec42be5803da420de3c1
Evgeniy_Grokholskiy <grokholskiy@gmail.com>
2021-10-30 12:30:09 +0500

    menu upgarde

--- js/header_menu.js
+++ js/header_menu.js
@@ -4,12 +4,14 @@ menuOpenCloseDomElement.addEventListener('click', classToggle);
 
 function classToggle () {
 
+    const cover = document.querySelector('.cover');
     const menuItemsNodeList = document.querySelectorAll('.header__list_item-mobile');
     const menuUlContainer = document.querySelector('.header__nav-mobile');
     const menuOpenCloseIcon = document.querySelector('.header__nav-arrow');
 
     menuOpenCloseIcon.classList.toggle('header__nav-arrow-open');
     menuUlContainer.classList.toggle('show');
+    cover.classList.toggle('cover-on');
 
     if (menuUlContainer.classList.contains('show')) {
         setTimeout(() => {
